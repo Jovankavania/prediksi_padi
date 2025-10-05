9cfd675e16fc6574d999d1a3a8f755c08788cdde
Jovania <144302207+Jovankavania@users.noreply.github.com>
2025-10-05 18:51:33 +0700

    Update prediksi_app.py

--- prediksi_padi/prediksi_app.py
+++ prediksi_padi/prediksi_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import joblib
 import altair as alt
+from prediksi_padi.src.clustering_prediksi import do_clustering
+
 
 st.title("🌾 Prediksi Produksi Padi – Growth Projection")
 
@@ -103,6 +105,14 @@ if uploaded_file is not None:
             .properties(width=400, height=300)
         )
         st.altair_chart(chart_total, use_container_width=True)
+
+        #Clustering
+        if st.button("🧩 Lakukan Segmentasi Hasil Prediksi"):
+            df_clustered, chart_cluster = do_clustering(df_proj)
+            st.write("### 📊 Hasil Segmentasi Kecamatan")
+            st.dataframe(df_clustered[["Kecamatan", "Cluster", "Prediksi Produksi"]])
+            st.altair_chart(chart_cluster, use_container_width=True)
+
 
 else:
     st.info("⬆️ Silakan unggah file Excel untuk mulai prediksi.")
